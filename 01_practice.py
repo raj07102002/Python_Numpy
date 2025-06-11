0be4a3ce9b6e1e07d879bb166a2bfaa1428c17bb
raj07102002 <rajchandrawanshi69@gmail.com>
2025-06-11 14:35:57 +0530

    o

--- 01_practice.py
+++ 01_practice.py
@@ -1,6 +1,11 @@
 # conversion of list into the array
-l=[]
-for i in range(1,6):
-    a=input("Enter the value")
-    l.append(a) 
-print(l)
+# l=[]
+# for i in range(1,6):
+#     a=input("Enter the value")
+#     l.append(a) 
+# print(l) 
+
+Q.2 
+import numpy as np
+arr=np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(arr)
