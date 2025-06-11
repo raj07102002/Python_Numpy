baed6f51c8f17e9523535a30240c2148d00d0a3c
raj07102002 <rajchandrawanshi69@gmail.com>
2025-06-11 21:19:50 +0530

    m

--- 01_practice.py
+++ 01_practice.py
@@ -21,3 +21,14 @@
 # b = a * 2
 
 # print(b)
+
+# Q.4 
+
+# import numpy as np
+
+# my_list = [10, 20, 30]           # square brackets → list
+# my_tuple = (1, 2)                # round brackets → tuple
+# my_dict = {"x": 100, "y": 200}   # curly braces → dictionary
+# my_array = np.array(my_list)    # use () to call function, pass list []
+
+# print(my_array[1])              # Access element with []
